a2e72d43cc8f6ed246601b71499004759274d9c2
Lucio Rodriguez <luonstreak@gmail.com>
2021-12-21 17:35:56 -0800

    update carousel

--- components/Carousel.js
+++ components/Carousel.js
@@ -14,12 +14,12 @@ const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
     items: 3,
-    slidesToSlide: 3 // optional, default to 1.
+    slidesToSlide: 1 // optional, default to 1.
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
     items: 2,
-    slidesToSlide: 2 // optional, default to 1.
+    slidesToSlide: 1 // optional, default to 1.
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
@@ -67,9 +67,10 @@ export default function MyCarousel(props) {
               ssr={true} // means to render carousel on server-side.
               infinite={true}
               autoPlay={props.deviceType !== "mobile" ? true : false}
-              autoPlaySpeed={3000}
+              slidesToSlide={1}
+              autoPlaySpeed={5000}
               keyBoardControl={true}
-              customTransition="all .5"
+              customTransition="transform .5s ease-out"
               transitionDuration={500}
               containerClass="carousel-container"
               removeArrowOnDeviceType={["tablet", "mobile"]}
